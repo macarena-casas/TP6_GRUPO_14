42bbfb8c47164ac5e0b92c9ebe82a70788c8fab5
AlejandroBeron <ale.frecuenci@gmail.com>
2024-10-02 19:18:58 -0300

    Agrego los paquetes faltantes, también la ventana de vista principal con el menu.

--- TP6_GRUPO_14/src/main/Principal.java
+++ TP6_GRUPO_14/src/main/Principal.java
@@ -3,12 +3,18 @@ package main;
 import dao.Personadao;
 import daoImpl.Personadaoimpl;
 import entidad.Persona;
+import presentacion.vista.Vista_principal;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		
 		Personadao perdao = new Personadaoimpl() ;
 		Persona persona1= new Persona("12345678","Juan", "Perez");
+		Vista_principal frame = new Vista_principal();
+		frame.setVisible(true);
+
+
 		
 		 boolean fila = perdao.insert(persona1);
 		
---
+++ TP6_GRUPO_14/src/presentacion/vista/Vista_principal.java
@@ -0,0 +1,44 @@
+package presentacion.vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class Vista_principal extends JFrame {
+
+	private JPanel contentPane;
+
+	
+	public Vista_principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnPersona = new JMenu("Persona");
+		menuBar.add(mnPersona);
+		
+		JMenuItem mntmAgregar = new JMenuItem("Agregar");
+		mnPersona.add(mntmAgregar);
+		
+		JMenuItem mntmModificar = new JMenuItem("Modificar");
+		mnPersona.add(mntmModificar);
+		
+		JMenuItem mntmEliminar = new JMenuItem("Eliminar");
+		mnPersona.add(mntmEliminar);
+		
+		JMenuItem mntmListar = new JMenuItem("Listar");
+		mnPersona.add(mntmListar);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}	
+}
